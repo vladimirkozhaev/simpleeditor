26f0be327802daabf84e37e043f964481f3d5576
vkozhaev <vkozhaev@gmail.com>
2017-06-21 17:25:19 +0300

    Add simple editor

---
+++ src/org/newlanguageservice/editor/MinimalEditor.java
@@ -0,0 +1,45 @@
+package org.newlanguageservice.editor;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.part.EditorPart;
+
+public class MinimalEditor extends EditorPart {
+    private Label contents;
+    public void createPartControl(Composite parent) {
+       contents = new Label(parent, SWT.NONE);
+       contents.setText("Minimal Editor");
+    }
+    public void init(IEditorSite site, IEditorInput input) {
+       setSite(site);
+       setInput(input);
+    }
+    public void setFocus() {
+       if (contents != null)
+          contents.setFocus();
+    }
+	@Override
+	public void doSave(IProgressMonitor monitor) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void doSaveAs() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public boolean isDirty() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isSaveAsAllowed() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+ }
