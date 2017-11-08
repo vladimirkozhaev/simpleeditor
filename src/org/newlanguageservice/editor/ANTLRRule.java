578d1e69522816d99be8481ddbcc1534bd368d8b
vkozhaev <vkozhaev@gmail.com>
2017-11-08 19:20:27 +0200

    Fix the changes

---
+++ src/org/newlanguageservice/editor/ANTLRRule.java
@@ -0,0 +1,39 @@
+package org.newlanguageservice.editor;
+
+import org.eclipse.jface.text.TextAttribute;
+import org.eclipse.jface.text.rules.ICharacterScanner;
+import org.eclipse.jface.text.rules.IPredicateRule;
+import org.eclipse.jface.text.rules.IToken;
+import org.eclipse.jface.text.rules.Token;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Display;
+
+public class ANTLRRule implements IPredicateRule {
+
+	IToken token=new Token(new TextAttribute(new Color(Display.getCurrent(), new RGB(139,0,139))));
+	
+	public ANTLRRule(IToken token) {
+		super();
+		this.token = token;
+	}
+
+	@Override
+	public IToken evaluate(ICharacterScanner scanner) {
+		// TODO Auto-generated method stub
+		return token;
+	}
+
+	@Override
+	public IToken getSuccessToken() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IToken evaluate(ICharacterScanner scanner, boolean resume) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
