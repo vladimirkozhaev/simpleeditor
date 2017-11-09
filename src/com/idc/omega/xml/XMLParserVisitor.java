dce2cb05c814d7fa3d84928a6feeeac9249b1122
vkozhaev <vkozhaev@gmail.com>
2017-11-09 17:04:48 +0200

    Fix the code folding

--- src/com/idc/omega/xml/XMLParserVisitor.java
+++ src/com/idc/omega/xml/XMLParserVisitor.java
@@ -2,12 +2,9 @@ package com.idc.omega.xml;
 
 import java.util.List;
 
-import org.antlr.v4.runtime.ANTLRErrorListener;
 import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.RecognitionException;
-import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.misc.Interval;
 import org.newlanguageservice.antlr.XMLLexer;
 import org.newlanguageservice.antlr.XMLParser;
 import org.newlanguageservice.antlr.XMLParser.ContentSequenceContext;
@@ -23,8 +20,16 @@ import org.newlanguageservice.antlr.XMLParserBaseVisitor;
 
 public class XMLParserVisitor extends XMLParserBaseVisitor<XMLNodeInfo> {
 
+	private boolean mDoNotAddEmptyNodes;
+
+	public XMLParserVisitor(boolean pAddEmptyNodes) {
+		super();
+		this.mDoNotAddEmptyNodes = pAddEmptyNodes;
+	}
+
 	@Override
 	public XMLNodeInfo visitDocument(DocumentContext pCtx) {
+
 		return visitElement(pCtx.elem);
 	}
 
@@ -33,10 +38,20 @@ public class XMLParserVisitor extends XMLParserBaseVisitor<XMLNodeInfo> {
 		if (pCtx == null) {
 			return null;
 		}
-		String text = pCtx.getText();
 		
+		String text = getFullText(pCtx);
+
+		if (mDoNotAddEmptyNodes && text.indexOf("\n") < 0 && text.indexOf("\r") < 0) {
+			return null;
+		}
+
 		String pNodeName = pCtx.elementName == null ? " " : pCtx.elementName.getText();
-		XMLNodeInfo parentNode = new XMLNodeInfo(pNodeName, pCtx.getStart().getStartIndex(), text.length());
+		int textLen = text.length();
+
+		if (mDoNotAddEmptyNodes) {
+			textLen += 1;
+		}
+		XMLNodeInfo parentNode = new XMLNodeInfo(pNodeName, pCtx.getStart().getStartIndex(), textLen);
 		pCtx.attribute().forEach(attr -> parentNode.getProperties().put(attr.attrName.getText(),
 				attr.attrValue.getText().substring(1, attr.attrValue.getText().length() - 1)));
 		if (pCtx.cont != null) {
@@ -52,15 +67,26 @@ public class XMLParserVisitor extends XMLParserBaseVisitor<XMLNodeInfo> {
 		return parentNode;
 	}
 
-	public static XMLNodeInfo makeNodesInfo(String pStr) throws Exception {
+	private String getFullText(ElementContext pCtx) {
+		int startIndex = pCtx.start.getStartIndex();
+		int stopIndex = pCtx.stop.getStopIndex();
+		Interval interval = new Interval(startIndex, stopIndex);
+		return pCtx.start.getInputStream().getText(interval);
+	}
+
+	public static XMLNodeInfo makeNodesInfo(String pStr, boolean pAddEmptyNodes) {
 		XMLLexer lexer = new XMLLexer(new ANTLRInputStream(pStr));
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		XMLParser parser = new XMLParser(tokens);
 
 		DocumentContext tree = parser.document();
-		XMLParserVisitor eval = new XMLParserVisitor();
+		XMLParserVisitor eval = new XMLParserVisitor(pAddEmptyNodes);
 		XMLNodeInfo info = eval.visitElement(tree.elem);
 		return info;
 
+	}
+
+	public static XMLNodeInfo makeNodesInfo(String pStr) {
+		return makeNodesInfo(pStr, false);
 	}
 }
